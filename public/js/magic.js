5c97ff6fac3708588393233e563c8dd912c00ed3
Kalvir Sandhu <kalv@kalv.co.uk>
2011-01-13 14:19:00 -0800

    stick a marker on the map from google spreadsheet

--- public/js/magic.js
+++ public/js/magic.js
@@ -1,3 +1,33 @@
+var geocoder = new google.maps.Geocoder();
+var map;
+
+function addCompany(entry) {
+  console.log(entry);
+
+  var address = entry.gsx$address_2.$t.replace(new RegExp("\\n", "g"), " ");
+
+  console.log(address);
+  geocoder.geocode( { 'address': address},
+    function(results, status) {
+      console.log(status);
+      if (status == google.maps.GeocoderStatus.OK) {
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        console.log("Geocode was not succesful")
+      }
+    }
+  );
+}
+
+function renderCompanies(json) {
+  $.each(json.feed.entry, function(i, entry) {
+    addCompany(entry)
+  });
+}
+
 $(document).ready(function() {
 
   var myLatlng = new google.maps.LatLng(49.278893, -123.115883);
@@ -9,7 +39,7 @@ $(document).ready(function() {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
 
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
   $("#form_link").click(function() {
     $("#form_container").show();
@@ -19,9 +49,5 @@ $(document).ready(function() {
   $("#cancel").click(function() {
     $("#form_container").hide();
     return false;
-  });
-
-  $("#form").load(function() {
-    console.log("load");
   });
 });
