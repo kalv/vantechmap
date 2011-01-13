a8ee281351065a9edc22c84b074592ff9a77e20f
Kalvir Sandhu <kalv@kalv.co.uk>
2011-01-13 11:33:47 -0800

    Added map and link to the iframe to show the spreadsheet form

--- Readme.md
+++ Readme.md
@@ -5,8 +5,8 @@ This is a just a little mash up using Google docs forms/spreadsheets to easily s
 I built this so that it would be easier for others to see what's going on in vancouver and where.
 
 ## Todo:
-- Show full screen map
-- Add a link to form or have slide out left panel for iframe
+
 - Pull and parse spreadsheet and render placeholders on the map
 - Show information when clicked
 - Google font to be the same as the form theme.
+- hide the iframe when a company is added
--- public/js/magic.js
+++ public/js/magic.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-  console.log("I'm going to run some magic");
+
+  var myLatlng = new google.maps.LatLng(49.278893, -123.115883);
+  var myOptions = {
+    zoom: 14,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+  $("#form_link").click(function() {
+    $("#form_container").show();
+    return false;
+  });
+
+  $("#cancel").click(function() {
+    $("#form_container").hide();
+    return false;
+  });
+
+  $("#form").load(function() {
+    console.log("load");
+  });
 });
